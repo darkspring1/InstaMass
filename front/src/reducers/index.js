200fee4ee628a4a9cbf242b8857a46cbdb252767
olt.egor@gmail.com <olt.egor@gmail.com>
2017-09-15 23:19:28 +0300

    reduser

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -2,8 +2,10 @@ import auth from './auth';
 import account from './account';
 import preloader from './preloader';
 import toastr from './toastr';
+import likeTask from './likeTask';
 
 export { auth };
 export { account };
 export { preloader };
 export { toastr };
+export { likeTask };
